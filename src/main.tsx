aef62a0d3b59948d95f279b1aba8f51419547f9d
աɨռɢӄաօռɢ <wingkwong.code@gmail.com>
2024-05-05 21:19:03 +0800

    feat(src): add global css

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom';
-import './index.css'
 import { Provider } from './provider.tsx'
+import "@/src/styles/globals.css"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
