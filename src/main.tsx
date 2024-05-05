a4c22eb929756be6582829cd311a65a72608b174
աɨռɢӄաօռɢ <wingkwong.code@gmail.com>
2024-05-05 21:45:38 +0800

    chore(src): update alias path

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from './provider.tsx'
-import "@/src/styles/globals.css"
+import "@/styles/globals.css"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
