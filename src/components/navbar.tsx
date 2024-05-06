8dee0e9a214b8bc44a343455281aaee210a504e3
աɨռɢӄաօռɢ <wingkwong.code@gmail.com>
2024-05-06 16:00:59 +0800

    refactor(src): move to individual packages

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,16 @@
+import { Button } from "@nextui-org/button";
+import { Kbd } from "@nextui-org/kbd";
+import { Link } from "@nextui-org/link";
+import { Input } from "@nextui-org/input";
 import {
-  Button,
-  Kbd,
-  Link,
-  Input,
   Navbar as NextUINavbar,
-  NavbarContent,
-  NavbarMenu,
-  NavbarMenuToggle,
   NavbarBrand,
+  NavbarContent,
   NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
   NavbarMenuItem,
-} from "@nextui-org/react";
+} from "@nextui-org/navbar";
 import { link as linkStyles } from "@nextui-org/theme";
 import clsx from "clsx";
 
--- src/provider.tsx
+++ src/provider.tsx
@@ -1,4 +1,4 @@
-import { NextUIProvider } from "@nextui-org/react";
+import { NextUIProvider } from "@nextui-org/system";
 import { useNavigate } from "react-router-dom";
 
 export function Provider({ children }: { children: React.ReactNode }) {
