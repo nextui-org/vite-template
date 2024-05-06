81290d124e2d52c7b73eaa52250da22a9330961e
աɨռɢӄաօռɢ <wingkwong.code@gmail.com>
2024-05-06 11:59:52 +0800

    chore(root): update content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,10 @@ import {nextui} from '@nextui-org/react'
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './layouts/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    "./index.html",
+    './src/layouts/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
