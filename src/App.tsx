d5dcdb2a8ea58905429c41e1de30d84f6e04239e
աɨռɢӄաօռɢ <wingkwong.code@gmail.com>
2024-05-06 12:31:53 +0800

    feat(src): add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
 import IndexPage from '@/pages/index';
+import DocsPage from '@/pages/docs';
+import PricingPage from '@/pages/pricing';
+import BlogPage from '@/pages/blog';
+import AboutPage from '@/pages/about';
 
 function App() {
 
   return (
     <Routes>
       <Route path='/' Component={IndexPage} />
+      <Route path='/docs' Component={DocsPage} />
+      <Route path='/pricing' Component={PricingPage} />
+      <Route path='/blog' Component={BlogPage} />
+      <Route path='/about' Component={AboutPage} />
     </Routes>
   )
 }
